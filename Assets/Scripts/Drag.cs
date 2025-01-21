3be94c4a81d0ea7357962884e03d7e9f599fd05f
ghostofgamer <105151923+ghostofgamer@users.noreply.github.com>
2025-01-21 14:53:25 +0400

    AddInput

--- Assets/Scripts/Drag.cs
+++ Assets/Scripts/Drag.cs
@@ -6,30 +6,40 @@ public class Drag : MonoBehaviour
     [SerializeField] private float _pickUpDistance = 5f;
     [SerializeField] private LayerMask _itemLayer;
     [SerializeField] private Transform _itemPosition;
-
+    
     private GameObject _pickedItem;
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        /*if (Input.GetMouseButtonDown(0))
         {
             if (_pickedItem == null)
                 PickUpItem();
             else
                 DropItem();
-        }
+        }*/
 
-        if (Input.GetMouseButtonDown(1))
+        /*if (Input.GetMouseButtonDown(1))
         {
             if (_pickedItem != null)
                 ThrowItem();
-        }
+        }*/
     }
 
-    private void PickUpItem()
+    public void PickUpItem()
     {
-        Ray ray = _playerCamera.ScreenPointToRay(Input.mousePosition);
+        Debug.Log("Picked up Item");
+        if (_pickedItem != null) return;
+        Debug.Log("Picked up !!!");
+        
+        Vector2 screenCenter = new Vector2(Screen.width / 2, Screen.height / 2);
+
+        // Бросаем рейкаст из центра экрана
+        Ray ray = _playerCamera.ScreenPointToRay(screenCenter);
         RaycastHit hit;
+        
+        /*Ray ray = _playerCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;*/
 
         if (Physics.Raycast(ray, out hit, _pickUpDistance, _itemLayer))
         {
@@ -40,14 +50,18 @@ public class Drag : MonoBehaviour
         }
     }
 
-    private void DropItem()
+    public void DropItem()
     {
+        if (_pickedItem == null) return;
+        
         _pickedItem.GetComponent<Rigidbody>().isKinematic = false;
         ClearPickedItem();
     }
 
-    private void ThrowItem()
+    public void ThrowItem()
     {
+        if (_pickedItem == null) return;
+        
         _pickedItem.GetComponent<Rigidbody>().isKinematic = false;
         _pickedItem.GetComponent<Rigidbody>().AddForce(_playerCamera.transform.forward * 10f, ForceMode.Impulse);
         ClearPickedItem();
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,17 +1,13 @@
 using UnityEngine;
 
-
 [RequireComponent(typeof(CharacterController))]
 public class Movement : MonoBehaviour
 {
-    private const string Horizontal = "Horizontal";
-    private const string Vertical = "Vertical";
-    private const string MouseX = "Mouse X";
-    private const string MouseY = "Mouse Y";
     private const string Jump = "Jump";
     
+    [SerializeField] private Joystick _joystick;
+    [SerializeField] private Joystick _rotationJoystick;
     [SerializeField]private float _walkSpeed = 5f; 
-    [SerializeField]private float _runSpeed = 10f; 
     [SerializeField]private float _jumpForce = 5f; 
     [SerializeField]private Transform _cameraTransform; 
     [SerializeField]private float _mouseSensitivity = 2f; 
@@ -28,16 +24,23 @@ public class Movement : MonoBehaviour
     private void Start()
     {
         _controller = GetComponent<CharacterController>();
-        Cursor.lockState = CursorLockMode.Locked;
+        // Cursor.lockState = CursorLockMode.Locked;
     }
 
+    public void Move(Vector2 movement)
+    {
+        _move = transform.right * -movement.x + transform.forward * -movement.y;
+        _controller.Move(_move * _walkSpeed * Time.deltaTime);
+    }
+    
+    
+    
     private void Update()
     {
-        _moveSpeed = Input.GetKey(KeyCode.LeftShift) ? _runSpeed : _walkSpeed;
-        float horizontal = Input.GetAxis(Horizontal);
-        float vertical = Input.GetAxis(Vertical);
+        /*float horizontal = _joystick.Horizontal;
+        float vertical = _joystick.Vertical;
         _move = transform.right * -horizontal + transform.forward * -vertical;
-        _controller.Move(_move * _moveSpeed * Time.deltaTime);
+        _controller.Move(_move * _walkSpeed * Time.deltaTime);*/
         
         if (_controller.isGrounded)
         {
@@ -53,8 +56,11 @@ public class Movement : MonoBehaviour
         
         _playerVelocity.y += Physics.gravity.y * Time.deltaTime;
         _controller.Move(_playerVelocity * Time.deltaTime);
-        _mouseX = Input.GetAxis(MouseX) * _mouseSensitivity;
-        _mouseY = Input.GetAxis(MouseY) * _mouseSensitivity;
+
+        /*_mouseX = _rotationJoystick.Horizontal*_mouseSensitivity;
+        _mouseY = _rotationJoystick.Vertical*_mouseSensitivity;
+        
+       
         transform.Rotate(Vector3.up * _mouseX);
 
         Vector3 currentRotation = _cameraTransform.rotation.eulerAngles;
@@ -64,6 +70,6 @@ public class Movement : MonoBehaviour
             desiredRotationX -= 360;
         
         desiredRotationX = Mathf.Clamp(desiredRotationX, -_angle, _angle);
-        _cameraTransform.rotation = Quaternion.Euler(desiredRotationX, currentRotation.y, currentRotation.z);
+        _cameraTransform.rotation = Quaternion.Euler(desiredRotationX, currentRotation.y, currentRotation.z);*/
     }
 }
